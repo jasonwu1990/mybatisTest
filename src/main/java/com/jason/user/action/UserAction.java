4b3f60da4076f2fca391f9d6846a70e6fd452f37
jasonwu1990 <jasonwu1990@163.com>
2016-12-07 16:51:29 +0800

    test controller

--- src/main/java/com/jason/user/action/UserAction.java
+++ src/main/java/com/jason/user/action/UserAction.java
@@ -1,8 +1,8 @@
-package com.jason.controller;
+package com.jason.user.action;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.jason.user.dto.User;
 import com.jason.user.service.IUserService;
 
-@Controller  
-@RequestMapping("/user")  
-public class BaseController {
-    @Resource  
+@Controller
+@RequestMapping("/avatar")
+public class UserAction{
+
+    @Autowired  
     private IUserService userService;  
       
     @RequestMapping("/showUser")
